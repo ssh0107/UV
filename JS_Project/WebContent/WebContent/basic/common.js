a149e9b969e6ffad162fae1b45ea699fd2e91206
ssh0107 <103502764+ssh0107@users.noreply.github.com>
2023-02-22 17:34:56 +0900

    Add files via upload

---
+++ JS_Project/WebContent/WebContent/basic/common.js
@@ -0,0 +1,5 @@
+function kosa3(){
+	alert('kosa3');
+	console.log(kosa3);
+	document.write('<h1>Kosa</h1>')
+}
---
+++ JS_Project/WebContent/WebContent/dom/scripts/showPic.js
@@ -0,0 +1,39 @@
+/*function showPic(obj){
+		let source= obj.getAttribute("href");
+		let placeholder = document.getElementById("placeholder");
+		placeholder.setAttribute("src",source);
+		
+		//imagegallery.preventDefault()
+	}*/ //내가 한 거
+
+
+	function showPic(obj){
+			let source= obj.getAttribute("href");
+			let placeholder = document.getElementById("placeholder");
+			placeholder.setAttribute("src",source);
+			
+			let text = obj.getAttribute("title");
+			let description = document.getElementById("description");
+			
+			if(description.firstChild.nodeType ==3){
+				description.firstChild.nodeValue = text;
+			}
+			//description.text = text;
+		}
+
+	function prepareGallery(){
+		let imagegallery = document.getElementById("imagegallery"); //ul
+		let links = imagegallery.getElementsByTagName("a") //a들
+
+	for(let i = 0;i<links.length;i++){
+		links[i].addEventListener('click', function(e){
+			e.preventDefault();		//기본 이벤트 취소
+			showPic(this); 
+		},false);
+		
+		
+	}
+	
+	}
+	
+	window.onload = prepareGallery;
